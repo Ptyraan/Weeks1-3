70118a2a9201bca7cd1e8a6a614aa31ef166b671
pangta <pangta@OA-S318-08>
2026-01-27 14:01:07 -0500

    some stuff

--- Assets/Scripts/LinearInterpolation.cs
+++ Assets/Scripts/LinearInterpolation.cs
@@ -5,6 +5,7 @@ public class LinearInterpolation : MonoBehaviour
     public Transform start;
     public Transform end;
     public float t = 0;
+    public float v = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,7 +16,7 @@ public class LinearInterpolation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        t += Time.deltaTime * 2;
+        t += Time.deltaTime * v;
         if (t > 1) {
             t = 0;
         }
